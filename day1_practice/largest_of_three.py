1d01b143e72015675d4af0df93cd50632913de0f
sanskriti-codes01 <sksikkarwar08@gmail.com>
2026-02-20 18:38:42 +0530

    Update largest_of_three.py

--- day1_practice/largest_of_three.py
+++ day1_practice/largest_of_three.py
@@ -1,1 +1,9 @@
-
+a = int(intput("Enter first number:"))
+b = int(intput("Enter second number:"))
+c = int(input("Enter third number:"))
+if a >= b and a >= c :
+  print(a)
+elif b >= a and b >= c:
+  print(b)
+else:
+  print(c)
